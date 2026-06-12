662170cf35f7709d7c34373474c952da9275d26a
khavutru <dunukhataiphet@gmail.com>
2026-06-12 22:41:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,41 @@ document.getElementById("welcome").innerText =
 
 }
 
+function createPost() {
+const content =
+document.getElementById("postContent").value;
+
+const nickname =
+    localStorage.getItem("dunukha_nickname") || "Ẩn danh";
+
+if (!content) {
+    alert("Hãy nhập nội dung bài viết");
+    return;
+}
+
+const post = document.createElement("div");
+
+post.className = "post";
+
+post.innerHTML = `
+    <h3>${nickname}</h3>
+    <p>${content}</p>
+    <p>❤️ 0 | 🤝 0 | 💡 0 | 🌱 0</p>
+`;
+
+document
+    .getElementById("posts")
+    .prepend(post);
+
+document
+    .getElementById("postContent")
+    .value = "";
+
+}
+
 window.onload = function () {
-const nickname = localStorage.getItem("dunukha_nickname");
+const nickname =
+localStorage.getItem("dunukha_nickname");
 
 if (nickname) {
     document.getElementById("welcome").innerText =
