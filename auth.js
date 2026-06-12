dc10922742e8df80686388082bbcc1b92c7af07f
khavutru <dunukhataiphet@gmail.com>
2026-06-13 02:29:02 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,130 @@
+import { auth, db } from "./firebase.js";
 
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+import {
+doc,
+setDoc
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+window.register = async function(){
+
+try{
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+if(!email || !password){
+alert("Nhập email và mật khẩu");
+return;
+}
+
+const userCredential =
+await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+const user =
+userCredential.user;
+
+await setDoc(
+doc(db,"users",user.uid),
+{
+uid:user.uid,
+email:user.email,
+nickname:user.email.split("@")[0],
+avatar:"🌱",
+bio:"",
+online:true,
+createdAt:new Date().toISOString()
+}
+);
+
+alert("Đăng ký thành công");
+
+}catch(error){
+
+alert(error.message);
+
+console.log(error);
+
+}
+
+};
+
+window.login = async function(){
+
+try{
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+alert("Đăng nhập thành công");
+
+}catch(error){
+
+alert(error.message);
+
+}
+
+};
+
+window.logoutUser = async function(){
+
+try{
+
+await signOut(auth);
+
+alert("Đăng xuất thành công");
+
+}catch(error){
+
+alert(error.message);
+
+}
+
+};
+
+onAuthStateChanged(
+auth,
+(user)=>{
+
+const status =
+document.getElementById("authStatus");
+
+if(!status) return;
+
+if(user){
+
+status.innerHTML =
+"✅ Đang đăng nhập: " +
+user.email;
+
+}else{
+
+status.innerHTML =
+"❌ Chưa đăng nhập";
+
+}
+
+}
+);
