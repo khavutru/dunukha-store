4caea21b5e40a22c65efe95846e44d9b1c9c40b3
khavutru <dunukhataiphet@gmail.com>
2026-06-13 00:12:28 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,25 +1,26 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 
 import {
-  getAuth
+getAuth
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 import {
-  getFirestore
+getFirestore
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "dunukha-community.firebaseapp.com",
-  projectId: "dunukha-community",
-  storageBucket: "dunukha-community.firebasestorage.app",
-  messagingSenderId: "48190569970",
-  appId: "1:48190569970:web:7f0292bace5d342cd33f6b"
+apiKey: "AIzaSyCDLlBCUXLxSXb3n4ubjv40EM2jpcodA-c",
+authDomain: "dunukha-community.firebaseapp.com",
+projectId: "dunukha-community",
+storageBucket: "dunukha-community.firebasestorage.app",
+messagingSenderId: "48190569970",
+appId: "1:48190569970:web:7f0292bace5d342cd33f6b"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
 
-console.log("Firebase Ready");
+console.log("🔥 Firebase Connected");
