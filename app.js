f3d51f0e936aab485fa7a03277ce157247039b7b
khavutru <dunukhataiphet@gmail.com>
2026-06-12 22:36:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+function saveNickname() {
+const nickname = document.getElementById("nickname").value;
 
+if (!nickname) {
+    alert("Vui lòng nhập nickname");
+    return;
+}
+
+localStorage.setItem("dunukha_nickname", nickname);
+
+document.getElementById("welcome").innerText =
+    "Xin chào " + nickname + "!";
+
+}
+
+window.onload = function () {
+const nickname = localStorage.getItem("dunukha_nickname");
+
+if (nickname) {
+    document.getElementById("welcome").innerText =
+        "Xin chào " + nickname + "!";
+}
+
+};
