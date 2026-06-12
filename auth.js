9949d5a2495bc49c1ac6fa300e06c9afbfd7e47b
khavutru <dunukhataiphet@gmail.com>
2026-06-12 23:46:26 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,13 @@
+let currentUser = null;
 
+function register(email, password) {
+console.log("Register:", email);
+}
+
+function login(email, password) {
+console.log("Login:", email);
+}
+
+function logout() {
+currentUser = null;
+}
