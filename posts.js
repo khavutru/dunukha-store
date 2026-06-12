b84cc0713fd36cf2b7afb44127e9c04b1f0e98a6
khavutru <dunukhataiphet@gmail.com>
2026-06-13 00:44:27 +0700

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,18 +1,92 @@
-function createPost(content) {
+import { db, auth } from "./firebase.js";
 
-let posts =
-JSON.parse(
-localStorage.getItem("dunukha_posts") || "[]"
+import {
+collection,
+addDoc,
+getDocs,
+query,
+orderBy,
+serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+window.createPost = async function () {
+
+const content =
+document.getElementById("postContent").value;
+
+if (!content) {
+alert("Nhập nội dung");
+return;
+}
+
+const user = auth.currentUser;
+
+if (!user) {
+alert("Hãy đăng nhập");
+return;
+}
+
+await addDoc(
+collection(db, "posts"),
+{
+userId: user.uid,
+email: user.email,
+nickname: user.email.split("@")[0],
+content: content,
+likes: 0,
+createdAt: serverTimestamp()
+}
 );
 
-posts.unshift({
-id: Date.now(),
-content,
-createdAt: new Date()
+document.getElementById("postContent").value = "";
+
+alert("Đăng bài thành công");
+
+loadPosts();
+
+};
+
+window.loadPosts = async function () {
+
+const postsDiv =
+document.getElementById("posts");
+
+const q =
+query(
+collection(db, "posts"),
+orderBy("createdAt", "desc")
+);
+
+const snapshot =
+await getDocs(q);
+
+let html = "";
+
+snapshot.forEach(doc => {
+
+const post = doc.data();
+
+html += `
+<div class="post">
+
+<h3>🌌 ${post.nickname}</h3>
+
+<p>${post.content}</p>
+
+<hr>
+
+❤️ ${post.likes || 0}
+
+</div>
+`;
+
 });
 
-localStorage.setItem(
-"dunukha_posts",
-JSON.stringify(posts)
+postsDiv.innerHTML = html;
+
+};
+
+window.addEventListener(
+"load",
+loadPosts
 );
-}
