8435ac6c3441c76fde2a60bdfb0c7c8d006cb590
khavutru <dunukhataiphet@gmail.com>
2026-06-12 23:47:46 +0700

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,1 +1,18 @@
+function createPost(content) {
 
+let posts =
+JSON.parse(
+localStorage.getItem("dunukha_posts") || "[]"
+);
+
+posts.unshift({
+id: Date.now(),
+content,
+createdAt: new Date()
+});
+
+localStorage.setItem(
+"dunukha_posts",
+JSON.stringify(posts)
+);
+}
