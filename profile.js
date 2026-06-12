910897690c849868068685ce48ea2b591de4b34c
khavutru <dunukhataiphet@gmail.com>
2026-06-13 01:18:28 +0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,13 +1,60 @@
-const profile = {
-nickname: "",
-bio: "",
-badge: "",
-avatar: ""
+import { auth, db } from "./firebase.js";
+
+import {
+doc,
+getDoc,
+updateDoc
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+window.saveProfile = async function() {
+
+const user = auth.currentUser;
+
+if(!user){
+alert("Đăng nhập trước");
+return;
+}
+
+await updateDoc(
+doc(db,"users",user.uid),
+{
+nickname:
+document.getElementById("nickname").value,
+
+avatar:
+document.getElementById("avatar").value,
+
+bio:
+document.getElementById("bio").value
+}
+);
+
+alert("Lưu hồ sơ thành công");
+
 };
 
-function saveProfile(data) {
-localStorage.setItem(
-"dunukha_profile",
-JSON.stringify(data)
+window.loadProfile = async function() {
+
+const user = auth.currentUser;
+
+if(!user) return;
+
+const snap =
+await getDoc(
+doc(db,"users",user.uid)
 );
-}
+
+if(!snap.exists()) return;
+
+const data = snap.data();
+
+document.getElementById("nickname").value =
+data.nickname || "";
+
+document.getElementById("avatar").value =
+data.avatar || "";
+
+document.getElementById("bio").value =
+data.bio || "";
+
+};
