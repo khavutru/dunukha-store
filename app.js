d8071ce74e0624526e76b5ba8c7d2473b8615a65
khavutru <dunukhataiphet@gmail.com>
2026-06-13 10:02:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+alert("APP LOADED");
+
 import { auth, db } from "./firebase.js";
+
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+import {
+doc,
+setDoc,
+getDoc,
+collection,
+addDoc,
+getDocs,
+query,
+orderBy,
+serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 import {
 createUserWithEmailAndPassword,
