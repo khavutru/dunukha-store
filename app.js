02c72054f3ec4d265c758a7fd5579a92b2164687
khavutru <dunukhataiphet@gmail.com>
2026-06-13 10:17:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -18,3 +18,103 @@ query,
 orderBy,
 serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+window.register = async function(){
+
+try{
+
+const email =
+document.getElementById("email").value.trim();
+
+const password =
+document.getElementById("password").value.trim();
+
+const result =
+await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+const user =
+result.user;
+
+await setDoc(
+doc(db,"users",user.uid),
+{
+uid:user.uid,
+email:user.email,
+nickname:user.email.split("@")[0],
+bio:""
+}
+);
+
+alert("Đăng ký thành công");
+
+}catch(error){
+
+alert(error.message);
+
+}
+
+};
+
+window.login = async function(){
+
+try{
+
+const email =
+document.getElementById("email").value.trim();
+
+const password =
+document.getElementById("password").value.trim();
+
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+}catch(error){
+
+alert(error.message);
+
+}
+
+};
+
+window.logoutUser = async function(){
+
+await signOut(auth);
+
+};
+
+onAuthStateChanged(auth,(user)=>{
+
+const loginPage =
+document.getElementById("loginPage");
+
+const appPage =
+document.getElementById("appPage");
+
+if(!loginPage || !appPage) return;
+
+if(user){
+
+loginPage.style.display =
+"none";
+
+appPage.style.display =
+"block";
+
+}else{
+
+loginPage.style.display =
+"flex";
+
+appPage.style.display =
+"none";
+
+}
+
+});
